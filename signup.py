8c80826d8605f737670494a3d7ede705d4d92443
Mohit Chaudhary <mohitchaudhary0514@gmail.com>
2025-04-28 20:53:57 +0545

    added signup

---
+++ signup.py
@@ -0,0 +1,9 @@
+def signup(db, username, password):
+    user = db.get_user(username)
+    if user:
+        print("User Already register")
+        return
+    
+    db.add_user(username, password)
+    print("Signup Sucessfull")
+
